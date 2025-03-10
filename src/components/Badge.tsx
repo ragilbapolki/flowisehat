89c6b166557f2f277a184ea7d7c913bde2a27a23
Ragil Bapolki <ragil.bapolki.putra@gmail.com>
2025-03-10 10:39:54 +0700

    first commit

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -67,7 +67,7 @@ export const Badge = (props: Props) => {
           color: props.poweredByTextColor ?? defaultTextColor,
         }}
       >
-        {/* <span> Leon van Zyl</span> */}
+        {/* <span> </span> */}
       </a>
     </span>
   );
